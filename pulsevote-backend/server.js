c4b764803f8ff529f2fd39eea746640fca0635b6
Ashford Mc Harlies <ST10079154@vcconnect.edu.za>
2025-09-25 23:29:34 +0200

    05 - Securing your login

--- pulsevote-backend/server.js
+++ pulsevote-backend/server.js
@@ -4,16 +4,12 @@ const https = require('https');
 const fs = require('fs');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 const options = {
   key: fs.readFileSync('ssl/key.pem'),
   cert: fs.readFileSync('ssl/cert.pem'),
 };
-
-https.createServer(options, app).listen(PORT, () => {
-  console.log('Server running at https://localhost:' + PORT);
-});
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
